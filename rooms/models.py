268241238e0ed1c0b99709c7a106dfca179fe8f6
jaws <order18291@gmail.com>
2021-12-23 02:22:43 +0900

    form_으로_뷰완성

--- rooms/models.py
+++ rooms/models.py
@@ -53,7 +53,7 @@ class Room(core_models.TimeStampModel):
     city = models.CharField(max_length=80)
     price = models.IntegerField()
     address = models.CharField(max_length=140)
-    guest = models.IntegerField()
+    guest = models.IntegerField(help_text="얼마나 많은 사람들이 지낼껀지")
     beds = models.IntegerField()
     bedrooms = models.IntegerField()
     baths = models.IntegerField()
@@ -63,7 +63,9 @@ class Room(core_models.TimeStampModel):
     host = models.ForeignKey(
         user_models.User, related_name="rooms", on_delete=models.CASCADE
     )
-    room_type = models.ForeignKey(RoomType, related_name="rooms", on_delete=models.SET_NULL, null=True)
+    room_type = models.ForeignKey(
+        RoomType, related_name="rooms", on_delete=models.SET_NULL, null=True
+    )
     amenities = models.ManyToManyField(Amenity, related_name="rooms", blank=True)
     facilities = models.ManyToManyField(Facility, related_name="rooms", blank=True)
     house_rules = models.ManyToManyField(HouseRule, related_name="rooms", blank=True)
@@ -71,20 +73,20 @@ class Room(core_models.TimeStampModel):
     def __str__(self):
         return self.name
 
-    #이걸로 city에 소문자로 적든 대문자로 적든 무조건 첫 글자는 대문자로 적게됨 상속이용해서
+    # 이걸로 city에 소문자로 적든 대문자로 적든 무조건 첫 글자는 대문자로 적게됨 상속이용해서
     def save(self, *args, **kwargs):
         self.city = str.capitalize(self.city)
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return reverse("rooms:detail", kwargs={'pk':self.pk})
+        return reverse("rooms:detail", kwargs={"pk": self.pk})
 
     def total_rating(self):
         all_reviews = self.reviews.all()
         all_ratings = 0
         if len(all_reviews) > 0:
             for review in all_reviews:
-                all_ratings =+ review.rating_average()
+                all_ratings = +review.rating_average()
             return round(all_ratings / len(all_reviews))
 
 
@@ -92,9 +94,8 @@ class Photo(core_models.TimeStampModel):
     """photo model def"""
 
     caption = models.CharField(max_length=80)
-    file = models.ImageField(upload_to='room_photos')
+    file = models.ImageField(upload_to="room_photos")
     room = models.ForeignKey(Room, related_name="photos", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.caption
-
--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
-
-# from django.urls.base import reverse
-# from django.core.paginator import EmptyPage, Paginator
 from django.views.generic import ListView, DetailView
-
-# from django.utils import timezone
-# from django.http import Http404
-from django_countries import countries
-
-
+from . import forms
 from . import models
+from django.core.paginator import Paginator
 
 # Create your views here.
 # def all_rooms(request):
@@ -56,93 +49,70 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    city = request.GET.get("city", "Anywhere")
-    # anywhere는 파라미터가 아예 없을떄 뜸
-    city = str.capitalize(city)
-    country = request.GET.get("country", "KR")
-    room_type = int(request.GET.get("room_type", 0))
-    price = int(request.GET.get("price", 0))
-    guests = int(request.GET.get("guests", 0))
-    bedrooms = int(request.GET.get("bedrooms", 0))
-    beds = int(request.GET.get("beds", 0))
-    baths = int(request.GET.get("baths", 0))
-    instant = bool(request.GET.get("instant", False))
-    super_host = bool(request.GET.get("super_host", False))
-    print(f"인스턴트,슈퍼호스트 + {instant}, {super_host}")
-    s_amenities = request.GET.getlist("amenities")
-    s_facilities = request.GET.getlist("facilities")
-    print(f"어메니티,퍼실리티 + {s_amenities}, {s_facilities}")
 
-    form = {
-        "city": city,
-        "s_room_type": room_type,
-        "s_country": country,
-        "price": price,
-        "guests": guests,
-        "bedrooms": bedrooms,
-        "beds": beds,
-        "baths": baths,
-        "instant": instant,
-        "super_host": super_host,
-        "s_amenities": s_amenities,
-        "s_facilities": s_facilities,
-    }
-    room_types = models.RoomType.objects.all()
-    amenities = models.Amenity.objects.all()
-    facilities = models.Facility.objects.all()
+    country = request.GET.get("country")
 
-    choices = {
-        "countries": countries,
-        "room_types": room_types,
-        "amenities": amenities,
-        "facilities": facilities,
-    }
+    if country:
+        form = forms.SearchForm(request.GET)
+        if form.is_valid():
+            city = form.cleaned_data.get("city")
+            country = form.cleaned_data.get("country")
+            room_type = form.cleaned_data.get("room_type")
+            price = form.cleaned_data.get("price")
+            guests = form.cleaned_data.get("guests")
+            bedrooms = form.cleaned_data.get("bedrooms")
+            beds = form.cleaned_data.get("beds")
+            baths = form.cleaned_data.get("baths")
+            instant_book = form.cleaned_data.get("instant_book")
+            superhost = form.cleaned_data.get("superhost")
+            amenities = form.cleaned_data.get("amenities")
+            facilities = form.cleaned_data.get("facilities")
 
-    filter_args = {}
+            filter_args = {}
 
-    if city != "Anywhere":
-        filter_args["city__startswith"] = city
+            if city != "Anywhere":
+                filter_args["city__startswith"] = city
 
-    print(filter_args)
+            filter_args["country"] = country
 
-    filter_args["country"] = country
+            if room_type is not None:
+                filter_args["room_type"] = room_type
 
-    if room_type != 0:
-        filter_args["room_type__pk"] = room_type
+            if price is not None:
+                filter_args["price__lte"] = price
 
-    if price != 0:
-        filter_args["price__lte"] = price
+            if guests is not None:
+                filter_args["guests__gte"] = guests
 
-    if guests != 0:
-        filter_args["guests__gte"] = guests
+            if bedrooms is not None:
+                filter_args["bedrooms__gte"] = bedrooms
 
-    if bedrooms != 0:
-        filter_args["bedrooms__gte"] = bedrooms
+            if beds is not None:
+                filter_args["beds__gte"] = beds
 
-    if beds != 0:
-        filter_args["beds__gte"] = beds
+            if baths is not None:
+                filter_args["beths__gte"] = baths
 
-    if baths != 0:
-        filter_args["beths__gte"] = baths
+            if instant_book is True:
+                filter_args["instant_book"] = True
 
-    if instant is True:
-        filter_args["instant_book"] = True
+            if superhost is True:
+                filter_args["host__is_superhost"] = True
 
-    if super_host is True:
-        filter_args["host__is_superhost"] = True
+            for amenity in amenities:
+                filter_args["amenities"] = amenity
+                
+            for facility in facilities:
+                filter_args["facilities"] = facility
 
-    if len(s_amenities) > 0:
-        for s_amenity in s_amenities:
-            filter_args["amenities__pk"] = int(s_amenity)
-            
-    if len(s_facilities) > 0:
-        for s_facility in s_facilities:
-            filter_args["facilities__pk"] = int(s_facility)
+            qs = models.Room.objects.filter(**filter_args).order_by("-created")
+            paginator = Paginator(qs, 10, orphans=5)
+            page = request.GET.get("page", 1)
+            rooms = paginator.get_page(page)
 
-
-    print(s_amenities)
-
-    rooms = models.Room.objects.filter(**filter_args)
-    print(rooms)
-
-    return render(request, "rooms/search.html", {**form, **choices, "rooms": rooms})
+            return render(request, "rooms/search.html", {"form": form, "rooms": rooms})
+    else:
+        form = forms.SearchForm()
+    
+    return render(request, "rooms/search.html", {"form": form})
+    # 요걸로 장난치는 사람이 있어요 그래서 랜더링을 해줘야 해요↑
