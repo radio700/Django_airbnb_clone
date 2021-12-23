07cc7812649dfe20cd1412e2eae58e01df4f9c31
jaws <order18291@gmail.com>
2021-12-24 00:25:53 +0900

    modelform_이용한_회원가입

--- users/forms.py
+++ users/forms.py
@@ -17,24 +17,13 @@ class LoginForm(forms.Form):
         except models.User.DoesNotExist:
             self.add_error("email",forms.ValidationError("유저가 존재하지 않아여"))
 
-class SignupForm(forms.Form):
-    # class Meta:
-    #     model = models.User
-    #     fields = ("first_name","last_name","email","password")
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name","last_name","email","password")
 
-    first_name = forms.CharField(max_length=80)
-    last_name = forms.CharField(max_length=80)
-    email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
     password1 = forms.CharField(widget=forms.PasswordInput,label='패스확인')
-
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get(email=email)
-            raise forms.ValidationError("User already exists with that email")
-        except models.User.DoesNotExist:
-            return email
 
     def clean_password1(self):
         password = self.cleaned_data.get("password")
@@ -44,16 +33,15 @@ class SignupForm(forms.Form):
         else:
             return password
 
-    def save(self):
-        first_name = self.cleaned_data.get("first_name")
-        last_name = self.cleaned_data.get("last_name")
+    def save(self,*args,**kwargs):
+        user = super().save(commit=False)
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
-
-        user = models.User.objects.create_user(email,email=email,password=password)
-        user.first_name = first_name
-        user.last_name = last_name
+        user.username = email
+        user.set_password(password)
         user.save()
+
+
 
 
 
