88d0c6867724106342cea6e3f914335bc0e11bea
jaws <order18291@gmail.com>
2021-12-24 00:06:57 +0900

    회원가입_순수_form으로만듬

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from django.views import View
+from django.views.generic import FormView
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse_lazy
 from . import forms
-
 # Create your views here.
 
 class LoginView(View):
@@ -15,12 +16,32 @@ class LoginView(View):
         if form.is_valid():
             email = form.cleaned_data.get("email")
             password = form.cleaned_data.get("password")
-            user = authenticate(request, username=email, password=password)
+            user = authenticate(self.request, username=email, password=password)
             if user is not None:
-                login(request, user)
+                login(self.request, user)
                 return redirect("core:home")
         return render(request, "users/login.html", {"form": form})
 
 def log_out(request):
     logout(request)
     return redirect("core:home")
+
+class SignupView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignupForm
+    success_url = reverse_lazy("core:home")
+    initial ={
+        'first_name':'kim',
+        'last_name':'sejung',
+        'email':'radio700@hanmail.net',
+    }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
